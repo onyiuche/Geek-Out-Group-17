fd6470f99e4fa49edea6369b3118adc080c6eca0
ONYI UCHE <ouche@terpmail.umd.edu>
2017-11-04 18:32:46 -0400

    initial commit

---
+++ app/src/main/java/com/example/onyi/geekoutapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.onyi.geekoutapp;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        RelativeLayout rl = (RelativeLayout)findViewById(R.id.layout_id);
+//        rl.setBackgroundColor(Color.BLACK);
+    }
+}
